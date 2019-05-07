c73ddfe546d7d0522dc5d77775a2d0899fd55cbf
Nadiia <Nadiia@DESKTOP-5EC2TJO>
2019-05-07 11:18:43 +0300

    change Layout page

---
+++ Tilo/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tilo.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category name")]
+        public string Name { get; set; }
+    }
+}
